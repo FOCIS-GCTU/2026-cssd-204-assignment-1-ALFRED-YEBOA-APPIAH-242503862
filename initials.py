0d338be0c225c168a07fe132d638c7e828d762b9
ALFRED YEBOAH PPIAH 2425403862 <2425403862@live.gctu.edu.gh>
2026-06-02 19:13:03 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: ALFRED YEBOAH APPIAH
+# STUDENT ID: 2425403862
+# Email: 2425403862@live.gctu.edu.gh
+# Grader: MR.AUGUSTUS BUCKMAN
+#
+# On my honor, ALFRED YEBOAH APPIAH, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print large stylized initials
 
+    print()
+    print("...AYA")
+    print()
+
+    print("...AAAAAAAAAAAA........YYY.....YYYY........AAAAAAAAAAAA****")
+    print("...AAAA....AAAA........YYY.....YYYY........AAAA....AAAA****")
+    print("...AAAA....AAAA........YYY.....YYYY........AAAA....AAAA****")
+    print("...AAAA....AAAA........YYYY....YYYY........AAAA....AAAA****")
+    print("...AAAAAAAAAAAA........YYYYYYYYYYYY........AAAAAAAAAAAA****")
+    print("...AAAA....AAAA............YYY.............AAAA....AAAA****")
+    print("...AAAA....AAAA............YYY.............AAAA....AAAA****")
+    print("...AAAA....AAAA............YYY.............AAAA....AAAA****")
+    print("...AAAA....AAAA...**.......YYY........**...AAAA....AAAA****")
+    print("...AAAA....AAAA...**.......YYY........**...AAAA....AAAA****")
+
+    print()
 
 
 main()
